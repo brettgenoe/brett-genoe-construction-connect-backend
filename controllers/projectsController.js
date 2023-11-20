a45252d38e43c0429afef9787e1640b757b99efd
brettgenoe <brettgenoe@gmail.com>
2023-11-20 15:44:21 -0800

    added new seed date to include long and lat]
    e
    ;

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -27,20 +27,52 @@ const getProjectById = async (req, res) => {
 };
 
 const createProject = async (req, res) => {
-    const { company_name, location, duration, description, carpenters_needed, electricians_needed, plumbers_needed, operators_needed, safety_needed } = req.body;
+    const {
+        company_name,
+        duration,
+        description,
+        carpenters_needed,
+        carpenters_description,
+        electricians_needed,
+        electricians_description,
+        plumbers_needed,
+        plumbers_description,
+        operators_needed,
+        operators_description,
+        safety_needed,
+        safety_description,
+        labours_needed,
+        labours_description,
+        email,
+        telephone,
+        long,
+        lat
+    } = req.body;
 
     try {
         const newProject = await knex('projects').insert({
             company_name,
-            location,
             duration,
             description,
-            manager_id: req.users.user_id,
+            manager_id: req.user_id,
             carpenters_needed,
+            carpenters_description,
             electricians_needed,
+            electricians_description,
             plumbers_needed,
+            plumbers_description,
             operators_needed,
+            operators_description,
+            labours_needed,
+            labours_description,
             safety_needed,
+            safety_description,
+            email,
+            telephone,
+            geo_data: {
+                type: "Point",
+                coordinates: [long, lat]
+            }
         });
 
         res.json({ project_id: newProject[0] });
--- migrations/20231115143151_create_projects_table.js
+++ migrations/20231115143151_create_projects_table.js
@@ -6,7 +6,6 @@ exports.up = function (knex) {
     return knex.schema.createTable('projects', function (table) {
         table.increments('project_id').primary();
         table.string('company_name').notNullable();
-        table.string('location').notNullable();
         table.integer('duration').notNullable();
         table.text('description');
         table.integer('manager_id')
@@ -14,13 +13,21 @@ exports.up = function (knex) {
             .references('users.user_id')
             .onUpdate('CASCADE')
             .onDelete('CASCADE');
-        // table.integer('manager_id').unsigned();
-        // table.foreign('manager_id').references('user_id').inTable('users');
         table.integer('carpenters_needed').notNullable();
+        table.text('carpenters_description');
         table.integer('electricians_needed').notNullable();
+        table.text('electricians_description');
         table.integer('plumbers_needed').notNullable();
+        table.text('plumbers_description');
         table.integer('operators_needed').notNullable();
+        table.text('operators_description');
+        table.integer('labours_needed').notNullable();
+        table.text('labours_description');
         table.integer('safety_needed').notNullable();
+        table.text('safety_description');
+        table.json('geo_data');
+        table.string('email');
+        table.string('telephone');
     });
 }
 
@@ -29,5 +36,5 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-    return knex.schema.dropTable('projects')
+    return knex.schema.dropTable('projects');
 };
